c9af19852127b0c5437900ceb687a51b9c49fc6a
Riansebastian <riansebastian354@gmail.com>
2025-08-08 08:46:28 +0700

    Add files via upload

---
+++ list/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+  <div class="container">
+    <div class="left">
+      <h1>Ryannzz</h1>
+      <p class="hello">Hello</p>
+      <h2 class="welcome">welcome to my list</h2>
+      <p class="desc">This is where I add, edit, and delete my lists.</p>
+      <a href="index.php" class="btn">Masuk</a>
+    </div>
+    <div class="right">
+      <img src="buku.png" alt="Notebook" />
+    </div>
+  </div>
+  <footer>Design By Rian | 2025</footer>
+</body>
+
+</html>
